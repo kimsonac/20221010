c5d1fd21d0268477115359b290bbfe292ad39ae6
kimsonac <sonac.kim@gmail.com>
2022-10-25 19:20:26 +0900

    Add Mouse rotation
    마우스 움직임에 따른 카메라 방향 변경 추가, 플레이어가 바라보는 방향으로 이동하는 메소드 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Vector3 playerPos;
+    private Vector3 camearaRot;
     private Rigidbody rd;
     [SerializeField]
     private float speed;
     [SerializeField]
+    private float mouseSpeed;
+    [SerializeField]
     private float tilt;
     [SerializeField]
     private float boost;
+   
     //[SerializeField]
     //private float positionPitchFactor = -2f;
     //[SerializeField]
@@ -23,21 +27,26 @@ public class PlayerController : MonoBehaviour
 
     //private float pitch, yaw, roll;
 
+    
     private void Awake()
     {
         rd = GetComponent<Rigidbody>();
+        playerPos = transform.position;
+        camearaRot = transform.eulerAngles;
     }
     private void Update()
     {
-        Move();
-        ProcessRotation();
+        // Move();
+        MouseRotation();
+        Move2();
     }
 
     private void Move()
     {
-        playerPos.x = Input.GetAxisRaw("Horizontal");
-        playerPos.z = Input.GetAxisRaw("Vertical");
-        //TODO: 스페이스 누르면 위로 올라가기
+        playerPos.x = Input.GetAxisRaw("Horizontal"); // 옆
+        playerPos.z = Input.GetAxisRaw("Vertical"); // 앞뒤
+
+        //TODO: 스페이스 누르면 위로 올라가기 > 슈팅2d
         //TODO: 회전값 조정...
 
         //Debug.Log(playerPos);
@@ -46,14 +55,25 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private void ProcessRotation()
+    // https://bloodstrawberry.tistory.com/603
+    private void MouseRotation()
     {
-        //pitch = (transform.position.z * positionPitchFactor) + (playerPos.z * controlPitchFactor);
-        //yaw = transform.localPosition.x * positionYawFactor;
-        //roll = playerPos.x * controlRollFactor;
+        camearaRot.y = -Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSpeed; // 마우스 위아래
+        camearaRot.x = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSpeed; // 마우스 양옆
 
-        //transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+        camearaRot.x = transform.eulerAngles.x + camearaRot.x;
+        camearaRot.y += camearaRot.y;
 
-        
+        yRot = Mathf.Clamp(-90, yRot, 90); // Clamp: 값의 범위를 제한하는 함수
+        // -90도~90도로 방향 제한
+
+        transform.eulerAngles = camearaRot;
+    }
+
+    // https://itadventure.tistory.com/390#:~:text=%EA%B7%B8%EB%9F%AC%EB%82%98%20%EB%82%B4%EA%B0%80%20%EB%B0%94%EB%9D%BC%EB%B3%B4%EB%8A%94%20%EB%B0%A9%ED%96%A5,%EC%9C%BC%EB%A1%9C%20%EC%9D%B4%EB%8F%99%ED%95%98%EB%8A%94%20%EA%B2%83%EC%9E%85%EB%8B%88%EB%8B%A4.&text=%EB%B0%94%EB%9D%BC%EB%B3%B4%EB%8A%94%20%EB%B0%A9%ED%96%A5%EA%B3%BC%20%EC%95%84%EC%A3%BC,%EC%9D%B4%EB%8F%99%ED%95%98%EB%8A%94%20%ED%82%A4%EB%A1%9C%20%EC%9D%B8%EC%8B%9D%ED%95%A9%EB%8B%88%EB%8B%A4.
+    private void Move2()
+    {
+        playerPos = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+        transform.position += playerPos * speed * Time.deltaTime;
     }
 }
